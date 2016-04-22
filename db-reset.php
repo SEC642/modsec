aba897cdc7274db50d02705de52f5c452cae34c1
meeas <github.com@meeas.com>
2016-04-22 17:54:41 -0600

    Fixed broken query

--- db-reset.php
+++ db-reset.php
@@ -1,15 +1,21 @@
 <?php
-$con = mysql_connect("localhost","root","");
-if (!$con)
-{
-    die(mysql_error());
-}
-$qry = 'TRUNCATE TABLE inputs; INSERT into inputs VALUES ("Justin Searle");';
-mysql_select_db("sqli", $con);
-if (!mysql_query($qry,$con))
-{
-    die(mysql_error());
-}
-echo "Database Reset";
-mysql_close($con);
+    $con = mysql_connect("localhost","root","");
+        if (!$con)
+            {
+            die(mysql_error());
+            }
+        $qry = 'TRUNCATE TABLE inputs;';
+            mysql_select_db("sqli", $con);
+            if (!mysql_query($qry,$con))
+                {
+                die(mysql_error());
+                }
+        $qry = 'INSERT into inputs VALUES ("Justin Searle");';
+            mysql_select_db("sqli", $con);
+            if (!mysql_query($qry,$con))
+                {
+                die(mysql_error());
+                }
+        echo "Database Reset";
+    mysql_close($con);
 ?>
