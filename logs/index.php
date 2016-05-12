586c77fd581b79568d788081fc2efd5848e02abc
meeas <github.com@meeas.com>
2016-05-12 11:27:41 -0600

    Added basic XSS and OSi protections

--- logs/index.php
+++ logs/index.php
@@ -1,3 +1,7 @@
+<html>
+<head></head>
+<body>
+
 <b>This page contains the last 10000 lines of the Apache error log.  Search for you IP address to find alerts from your own traffic.  The latest alerts are at the bottom.</b><br><br>
 
 <form action="index.php" method="post">
@@ -8,11 +12,17 @@
 <pre>
 <?php
 
-if (isset($_POST['search']))
-    {
-        $search = $_POST["search"];
-        system("grep $search /var/log/apache2/error.log");
+if (isset($_POST['search'])){
+    $search = $_POST["search"];
+    exec("grep ".escapeshellarg($search)." /var/log/apache2/error.log", $output);
+    foreach($output as $line){
+        echo htmlspecialchars($line), "\n";
     }
+}
 
 ?>
+
 </pre>
+
+</body>
+</html>
