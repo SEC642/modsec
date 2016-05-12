2b9734b726488975550b4597e1a6373a2f2ee245
meeas <github.com@meeas.com>
2016-05-12 10:15:54 -0600

    Added README and cleaned up repo

---
+++ README.md
@@ -0,0 +1,21 @@
+This is a basic web application to learn how ModSecurity works, providing both
+ModSecurity protected and ModSecurity disabled versions of a simple page with
+various vulnerabilities.  This application also exposes a browsable directory
+showing all the ModSecurity rules and configurations and a page that allows you
+to search through the Apache error logs to see ModSecurity block rules.
+
+# Install steps for Ubuntu 14.04
+
+    sudo -s
+    cd /var/www/html
+    rm index.html
+    git clone https://github.com/meeas/dojo-modsec.git ./
+    apt-get install apache2 libapache2-modsecurity mysql-server php5 php5-mysql
+    mysql -u root -p < modsec.sql
+    ln -s modsec.apache.conf /etc/apache2/sites-available/modsec.conf
+    a2dissite 000-default
+    a2ensite modsec
+    chmod a+x /var/log/apache2
+    chmod a+r /var/log/apache2/*
+    editor /etc/logrotate.d/apache2
+        ### change "create 640 root adm" to "create 644 root adm"
--- modsec.sql
+++ modsec.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.1.62, for debian-linux-gnu (x86_64)
+-- MySQL dump 10.13  Distrib 5.5.47, for debian-linux-gnu (x86_64)
 --
 -- Host: localhost    Database: sqli
 -- ------------------------------------------------------
--- Server version	5.1.62-0ubuntu0.11.10.1
+-- Server version	5.5.47-0ubuntu0.14.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -14,6 +14,14 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `sqli`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `sqli` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `sqli`;
 
 --
 -- Table structure for table `inputs`
@@ -46,4 +54,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-04-22 16:18:49
+-- Dump completed on 2016-05-12 16:08:34
