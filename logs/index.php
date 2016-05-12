36e6d72ad2bed44dbcef9961e76a52f77a8f74c5
meeas <github.com@meeas.com>
2016-05-12 11:55:00 -0600

    Fixed spelling/grammer

--- logs/index.php
+++ logs/index.php
@@ -2,7 +2,7 @@
 <head></head>
 <body>
 
-<b>This page contains the last 10000 lines of the Apache error log.  Search for you IP address or any other string to find alerts from your own traffic.  The latest alerts are at the top.</b><br><br>
+<b>This page contains the last 10000 lines of the Apache error log.  Search for your IP address or any other desired string to find alerts from your own traffic.  The latest alerts are at the top.</b><br><br>
 
 <form action="index.php" method="post">
     IP Address: <input type="text" name="search" value="<?php echo htmlspecialchars($_POST['search'])?>" />
