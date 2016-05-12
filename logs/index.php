d403da4e1196aa37f118e11dc780a706d131cb2f
meeas <github.com@meeas.com>
2016-05-12 11:53:20 -0600

    Keep last search term and reverse search results

--- logs/index.php
+++ logs/index.php
@@ -2,10 +2,10 @@
 <head></head>
 <body>
 
-<b>This page contains the last 10000 lines of the Apache error log.  Search for you IP address to find alerts from your own traffic.  The latest alerts are at the bottom.</b><br><br>
+<b>This page contains the last 10000 lines of the Apache error log.  Search for you IP address or any other string to find alerts from your own traffic.  The latest alerts are at the top.</b><br><br>
 
 <form action="index.php" method="post">
-    IP Address: <input type="text" name="search" />
+    IP Address: <input type="text" name="search" value="<?php echo htmlspecialchars($_POST['search'])?>" />
     <button type="submit">Submit</button>
 </form>
 
@@ -14,7 +14,7 @@
 
 if (isset($_POST['search'])){
     $search = $_POST["search"];
-    exec("grep ".escapeshellarg($search)." /var/log/apache2/error.log", $output);
+    exec("grep ".escapeshellarg($search)." /var/log/apache2/error.log | tac", $output);
     foreach($output as $line){
         echo htmlspecialchars($line), "\n";
     }
